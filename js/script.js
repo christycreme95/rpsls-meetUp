f723e9bbab58d65cf0328a3c7c637be0ba317cc8
Christy Eang <christyeang@yahoo.com>
2021-06-16 19:07:23 -0700

    new branch

--- js/script.js
+++ js/script.js
@@ -31,36 +31,92 @@ let cpuImg = '';
 let cpuAns = '';
 let userAns = '';
 let win = '';
+let rockLose = ['Paper','Spock'];
+let paperLose = ['Scissors', 'Lizard'];
+let scissorsLose = ['Rock','Spock'];
+let lizardLose = ['Scissors','Rock'];
+let spockLose = ['Lizard', 'Paper'];
 
 
 cpuBtn.addEventListener("click", function () {
-
+    getCPU();
+    setTimeout(function(){
+        gamePlay.classList.remove('d-none');
+    },500);
 });
 
 rock.addEventListener("click", function () {
+ userAns = "Rock";
+ console.log(userAns);
+ enter.classList.remove('d-none');
 
 });
 
 paper.addEventListener("click", function () {
+userAns = "Paper";
+console.log(userAns);
+enter.classList.remove('d-none');
 
 });
 
 scissors.addEventListener("click", function () {
-
+userAns = "Scissors";
+console.log(userAns);
+enter.classList.remove('d-none');
 });
 
 lizard.addEventListener("click", function () {
-
+userAns = "Lizard";
+console.log(userAns);
+enter.classList.remove('d-none');
 });
 
 spock.addEventListener("click", function () {
-
+userAns = "Spock";
+console.log(userAns);
+enter.classList.remove('d-none');
 });
 
 enter.addEventListener("click", function () {
-
+    // compare(userAns,cpuAns);
+    compare(userAns,cpuAns);
 });
 
 home.addEventListener("click", function() {
 
 });
+// https://csa2020studentapi.azurewebsites.net/rpsls
+async function getCPU(){
+    let promise = await fetch("https://csa2020studentapi.azurewebsites.net/rpsls");
+    cpuAns = await promise.text();
+    console.log(cpuAns);
+}
+
+
+function compare(user, cpu){
+    console.log("User: " +user, "CPU: "+ cpu);
+    gamePlay.classList.add('d-none');
+
+    if(user == cpu){
+        console.log("Tie");
+
+    }else if(user=="Rock" && !rockLose.includes(cpu)){
+        console.log("User Wins!");
+
+    }else if(user == "Paper" && !paperLose.includes(cpu)){
+        console.log("User Wins!");
+
+    }else if(user == "Scissors" && !scissorsLose.includes(cpu)){
+        console.log("User Wins!");
+
+    }else if(user == "Lizard" && !lizardLose.includes(cpu)){
+        console.log("User Wins!");
+
+    }else if(user == "Spock" && !spockLose.includes(cpu)){
+        console.log("User Wins!");
+    
+    }else{
+        console.log("CPU Wins");
+        
+    }
+}
